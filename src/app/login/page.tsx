30a9c585dd8375a543f82c00c188c49e870e79ed
dokoh <dokoh@dokohui-MacBookAir.local>
2023-11-08 20:22:31 +0900

    feat: 메인 페이지 수정 및 로그인 페이지 작성 #9

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,17 +4,37 @@ import Image from 'next/image'
 export default function login(){
     return (
         <div className="m-6">
-            <div className="text-4xl mt-3">strcat(*,*)</div>
-            <div className="text-4xl mt-2">//  스트링캣 로그인</div>
-            <div className="h-96 mt-20"></div>
-            <button className="bg-yellow-300 rounded-lg h-12 w-full">
-               <Image 
-               src="/kakao.png"
-               width={40}
-               height={40}
-               alt="kakao" />
-                <div>카카오로 시작하기</div>
-                </button>
+            <div className="text-4xl mt-3 font-FiraCode font-semibold">strcat(*,*)</div>
+            <div className="text-3xl mt-2 w-80 flex flex-row">
+                <div className="basis-2/12 font-FiraCode font-semibold">
+                    //
+                </div>
+                <div className="basis-5/12 font-FiraCode font-semibold">
+                    스트링캣
+                </div>
+                <div className="basis-3.5/12 font-FiraCode font-semibold bg-red-300">
+                     로그인
+                </div>
+            </div>
+            <div className="h-96 my-20"></div>
+            <button className="bg-yellow-300 rounded-lg h-12 w-full flex flex-row">
+                <Image 
+                src="/kakao.png"
+                width={20}
+                height={20}
+                alt="kakao"
+                className="basis-1/12 w-full h-full ml-16" />
+                <div className="basis-6/12 h-full w-full mt-3 text-left">카카오로 시작하기</div>
+            </button>
+            <button className="bg-neutral-200 mt-3 rounded-lg h-12 w-full flex flex-row">
+                <Image 
+                src="/Google.png"
+                width={20}
+                height={20}
+                alt="kakao"
+                className="basis-1/12 w-full h-full ml-16" />
+                <div className="basis-6/12 h-full mt-3 text-left">구글로 시작하기</div>
+            </button>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,17 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="m-6">
-        <div className="text-4xl mt-3">strcat(*,*)</div>
-        <div className="text-4xl mt-2">//  스트링캣</div>
-        <div className="text-xl mt-6">스트링캣은 문자열을 끝없이 늘려 스크롤을 만들 수 있는 신개념 롤링페이퍼 서비스 입니다.</div>
-        <div className="text-xl">소중한 사람에게 스트링캣을 남겨보세요!</div>
+        <div className="text-4xl mt-3 font-FiraCode font-semibold">strcat(*,*)</div>
+        <div className="text-3xl mt-2 flex flex-row">
+                <div className="basis-1/12 font-FiraCode font-semibold">
+                    //
+                </div>
+                <div className="basis-4/6 font-FiraCode font-semibold">
+                    스트링캣
+                </div>
+            </div>
+        <div className="text-xl mt-5 font-FiraCode">스트링캣은 문자열을 끝없이 늘려 스크롤을 만들 수 있는 신개념 롤링페이퍼 서비스 입니다.</div>
+        <div className="text-xl font-FiraCode">소중한 사람에게 스트링캣을 남겨보세요!</div>
         <div className="h-60"></div>
         <div className="text-2xl text-slate-400">
           <Link legacyBehavior href="/create">
