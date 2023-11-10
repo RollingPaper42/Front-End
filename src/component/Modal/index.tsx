7bbc139c0fe4204963c8935629417199732a12de
arkingco <arkingco@gmail.com>
2023-11-10 17:02:20 +0900

    feat: 전역 모달 스타일링 #10

--- src/component/Modal/index.tsx
+++ src/component/Modal/index.tsx
@@ -10,13 +10,22 @@ export default function Modal() {
     if (e.target !== e.currentTarget) return;
     setModal(null);
   };
+
+  const onClickClose = () => {
+    setModal(null);
+  };
   return (
     modal && (
       <div
-        className="fixed top-0 flex h-full w-full max-w-[calc(100vh*0.6)] items-center justify-center bg-slate-400 opacity-50"
+        className="fixed top-0 flex  h-full w-full max-w-[calc(100vh*0.6)] items-center justify-center bg-slate-400 opacity-50"
         onClick={onClickBackground}
       >
-        {modal.modalComponent}
+        <div className="relative flex h-[50%] w-[80%] flex-col items-center justify-center bg-red-300">
+          <button className=" m-2 self-end text-4xl" onClick={onClickClose}>
+            X
+          </button>
+          {modal.modalComponent}
+        </div>
       </div>
     )
   );
