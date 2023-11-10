fca76b05a7bb40da2dde6ae6085550aebb889df6
arkingco <arkingco@gmail.com>
2023-11-10 17:02:50 +0900

    feat: Error 모달 스타일링  #10

--- src/component/Modal/Error.tsx
+++ src/component/Modal/Error.tsx
@@ -3,5 +3,16 @@ interface Props {
 }
 
 export default function Error({ content }: Props) {
-  return <div className="h-[80%] w-[80%] bg-amber-400">{content}</div>;
+  return (
+    <div className=" flex h-[100%] w-[100%] flex-col bg-green-700">
+      <div className=" flex h-[70%] items-center justify-center text-4xl">
+        {content}
+      </div>
+      <div className=" flex h-[30%] justify-center">
+        <button className="h-[80%] w-[90%] bg-amber-200 text-3xl">
+          돌아가기
+        </button>
+      </div>
+    </div>
+  );
 }
