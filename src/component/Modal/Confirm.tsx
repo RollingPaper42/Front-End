f084986f2b37ee038ab9ee54f2626efcf588bf9a
arkingco <arkingco@gmail.com>
2023-11-10 17:02:34 +0900

    feat: confirm 모달 스타일링 #10

--- src/component/Modal/Confirm.tsx
+++ src/component/Modal/Confirm.tsx
@@ -6,10 +6,18 @@ interface Props {
 
 export default function Confirm({ content, yes, no }: Props) {
   return (
-    <div className=" h-[90%] w-[90%] bg-orange-900">
-      {content}
-      <button onClick={yes}> 예 </button>
-      <button onClick={no}> 아니요 </button>
+    <div className=" flex h-[100%] w-[100%] flex-col bg-green-700">
+      <div className=" flex h-[70%] items-center justify-center text-4xl">
+        {content}
+      </div>
+      <div className=" flex h-[30%] justify-around">
+        <button className="h-[80%] w-[40%] bg-amber-200 text-3xl" onClick={yes}>
+          예
+        </button>
+        <button className="h-[80%] w-[40%] bg-amber-200 text-3xl" onClick={no}>
+          아니요
+        </button>
+      </div>
     </div>
   );
 }
