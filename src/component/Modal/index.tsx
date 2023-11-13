f5cb9d3057531f092747f26cdbe298da59895bbf
arkingco <arkingco@gmail.com>
2023-11-13 13:05:26 +0900

    fix: background click 이벤트 삭제#10

--- src/component/Modal/index.tsx
+++ src/component/Modal/index.tsx
@@ -7,11 +7,6 @@ import { useRecoilState } from 'recoil';
 export default function Modal() {
   const [modal, setModal] = useRecoilState(modalState);
 
-  const onClickBackground = (e: any) => {
-    if (e.target !== e.currentTarget) return;
-    setModal(null);
-  };
-
   const onClickClose = () => {
     setModal(null);
   };
@@ -28,10 +23,7 @@ export default function Modal() {
 
   return (
     modal && (
-      <div
-        className="fixed top-0 flex  h-full w-full max-w-[calc(100vh*0.6)] items-center justify-center bg-slate-400 opacity-50"
-        onClick={onClickBackground}
-      >
+      <div className="fixed top-0 flex  h-full w-full max-w-[calc(100vh*0.6)] items-center justify-center bg-slate-400 opacity-50">
         <div className="relative flex h-[50%] w-[80%] flex-col items-center justify-center bg-red-300">
           <button className=" m-2 self-end text-4xl" onClick={onClickClose}>
             X
