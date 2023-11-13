32aa094d8765d449f240c4d30aa40ef0dc4c93eb
arkingco <arkingco@gmail.com>
2023-11-13 12:46:16 +0900

    feat: 모달 text 크기 및 align cetner 정렬#10

--- src/component/Modal/Error.tsx
+++ src/component/Modal/Error.tsx
@@ -5,7 +5,7 @@ interface Props {
 export default function Error({ content }: Props) {
   return (
     <div className=" flex h-[100%] w-[100%] flex-col bg-green-700">
-      <div className=" flex h-[70%] items-center justify-center text-4xl">
+      <div className=" flex h-[70%] items-center justify-center p-10  text-center text-2xl">
         {content}
       </div>
       <div className=" flex h-[30%] justify-center">
