1c00993a8408a092d0e7f5739ee576fdcfea9e6f
arkingco <arkingco@gmail.com>
2023-11-13 12:29:58 +0900

    feat: 고양이 gif 스트링캣 상세페이지 적용 #18

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import axios from 'axios';
+import Image from 'next/image';
 import { content } from '@/types/content';
 import { useEffect, useState } from 'react';
 import ObserveComponent from '@/component/ObserveComponent';
 
 export default function Home() {
   const [text, setTextState] = useState<content[]>([]);
+  const [cat, setCat] = useState<string>('/cat1.png');
+  const [catLocationX, setCatLocationX] = useState(0);
   useEffect(() => {
     axios
       .get('/api/text')
@@ -16,8 +19,26 @@ export default function Home() {
       .catch((error) => {});
   }, []);
 
+  useEffect(() => {
+    setCat('/catgif1.gif');
+    const catMove = setInterval(() => {
+      setCatLocationX((prev) => Math.random() * 500 + 1);
+    }, 1000);
+
+    return () => clearInterval(catMove);
+  }, []);
+
   return (
     <div className=" bg-lime-300">
+      <Image
+        className={` fixed`}
+        style={{ left: `${catLocationX}px` }}
+        src={`${cat}`}
+        alt="cat1"
+        width={100}
+        height={100}
+      />
+
       <div className=" p-5">
         {text.map((item) => {
           return (
