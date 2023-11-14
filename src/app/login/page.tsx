35d2411db40e5d15a272e2b19ce165e9dd6a7882
dokoh <dokoh@dokohui-MacBookAir.local>
2023-11-14 13:37:39 +0900

    feat/createpage_button#9 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,30 +1,33 @@
 'use client';
 
+import StrcatHeader from '@/component/StrcatHeader';
 import Image from 'next/image'
 
 export default function login(){
     return (
         <div className="m-6 relative">
-            <div className="text-4xl mt-3 font-FiraCode font-semibold">strcat(*,*)</div>
-            <div className="text-3xl mt-2 w-80 flex flex-row">
-                <div className="basis-2/12 font-FiraCode font-semibold">
-                    //
-                </div>
-                <div className="basis-5/12 font-FiraCode font-semibold">
-                    스트링캣
-                </div>
-                <div className="basis-3.5/12 font-FiraCode font-semibold bg-red-300">
-                     로그인
+            <div className="font-FiraCode font-semibold">
+                <StrcatHeader/>
+                <div className="text-3xl mt-2 w-80 flex flex-row">
+                    <div className="basis-2/12">
+                        //
+                    </div>
+                    <div className="basis-5/12">
+                        스트링캣
+                    </div>
+                    <div className="basis-3.5/12 bg-red-300">
+                        로그인
+                    </div>
                 </div>
             </div>
             <div className="h-96 my-20 flex flex-row justify-center items-center relative z-10">
-                <Image
+                {/* <Image
                     src="/strcatImage.png"
                     width={170}
                     height={100}
                     alt="strcatImage"
                     className='absolute opacity-10'
-                    />
+                    /> */}
             </div>
             <button className= "bg-yellow-300 mt-3 rounded-lg w-full h-12 flex flex-row justify-center ">
                 <div className=" basis-3/12"></div>
@@ -34,7 +37,7 @@ export default function login(){
                     width={48}
                     height={48}
                     alt="kakao"
-                    className="basis-13 h-12"/>
+                    className="h-12"/>
                     <div className= "basis-32 text-left h-full">카카오로 시작하기</div>
                 </div>
                 <div className="basis-3/12"></div>
@@ -47,7 +50,7 @@ export default function login(){
                     width={48}
                     height={48}
                     alt="kakao"
-                    className="basis-13 h-12"/>
+                    className="h-12"/>
                     <div className= "basis-32 text-left h-full">구글로 시작하기</div>
                 </div>
                 <div className="basis-3/12"></div>
