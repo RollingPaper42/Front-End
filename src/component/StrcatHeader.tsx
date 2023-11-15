ee277a2d6e74ffc32741d1732b54e75a10871a4e
dokoh <dokoh@dokohui-MacBookAir.local>
2023-11-15 16:55:10 +0900

    feat/메인 버튼 생성#27

--- src/component/StrcatHeader.tsx
+++ src/component/StrcatHeader.tsx
@@ -1,7 +1,32 @@
 'use client';
 
+import Image from 'next/image'
+import Link from 'next/link';
+
+// const [LoginState, SetLoginState] = useState(true)
 export default function StrcatHeader(){
     return (
-        <div className="text-2xl font-FiraCode font-semibold">strcat(*,*)</div>
+        <div className="flex flex-row">
+            {/* <div className="basis-32"> */}
+                <Link href="/">
+                    <Image
+                        src="/StrcatHeader.png"
+                        width={128}
+                        height={25}
+                        alt="StrcatHeader"
+                    />
+                </Link>
+             {/* </div> */}
+             <div className="basis-4/6"></div>
+       
+                <Link href="/login">
+                    <Image
+                        src="/LoginButton.png"
+                        width={74}
+                        height={34}
+                        alt="StrcatHeader"
+                    />
+                </Link>
+        </div>
     )
 }
