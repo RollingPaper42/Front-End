c614ea7919938360363272b4b5d042e7414026e5
arkingco <arkingco@gmail.com>
2023-11-15 13:47:25 +0900

    fix: useconfirm 이름 변경 #10

--- src/app/modal/page.tsx
+++ src/app/modal/page.tsx
@@ -1,29 +1,29 @@
 'use client';
 
 import Error from '@/component/Modal/Error';
-import { useConfirm } from '@/hooks/useConfirm';
 import { modalState } from '@/recoil/modal';
+import { confirm } from '@/utils/confirm';
 import { useRecoilState } from 'recoil';
 
 export default function Modal() {
   const [, setModal] = useRecoilState(modalState);
 
-  const onClickModal = () => {
+  const handleModal = () => {
     setModal({ modalComponent: <Error content="에러 메시지 글" /> });
   };
 
-  const onCLickConfirm = async () => {
-    const result = await useConfirm('컨펌 메시지 글', setModal);
-    console.log(result);
+  const handleConfirm = async () => {
+    const result = await confirm('컨펌 메시지 글', setModal);
+    // console.log(result);
   };
 
   return (
-    <div className="h-[100%] w-[100%] bg-green-200">
+    <div className="h-full w-full bg-green-200">
       <div>
-        <button onClick={onClickModal}>Modal click</button>
+        <button onClick={handleModal}>Modal click</button>
       </div>
       <div>
-        <button onClick={onCLickConfirm}>Confirm Click</button>
+        <button onClick={handleConfirm}>Confirm Click</button>
       </div>
     </div>
   );
--- src/utils/confirm.ts
+++ src/utils/confirm.ts
@@ -2,25 +2,23 @@ import Confirm from '@/component/Modal/Confirm';
 import { modalComponent } from '@/types/modalComponent';
 import { SetterOrUpdater } from 'recoil';
 
-export const useConfirm = (
+export const confirm = (
   content: string,
   setModal: SetterOrUpdater<modalComponent | null>,
 ): Promise<boolean> => {
   return new Promise((resolve) => {
     setModal({
-      modalComponent: (
-        <Confirm
-          content={content}
-          yes={() => {
-            setModal(null);
-            resolve(true);
-          }}
-          no={() => {
-            setModal(null);
-            resolve(false);
-          }}
-        />
-      ),
+      modalComponent: Confirm({
+        content: content,
+        yes: () => {
+          setModal(null);
+          resolve(true);
+        },
+        no: () => {
+          setModal(null);
+          resolve(false);
+        },
+      }),
     });
   });
 };
