2a08f2a9eef44eb2f42b161db2a4784209f213da
arkingco <arkingco@gmail.com>
2023-11-17 19:09:16 +0900

    fix: 모달 background 클릭 시 종료 이벤트 추가 #34

--- src/component/Modal/index.tsx
+++ src/component/Modal/index.tsx
@@ -1,26 +1,24 @@
 'use client';
 
+import useModal from '@/hooks/useModal';
 import { modalState } from '@/recoil/modal';
-import { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
 
 export default function Modal() {
-  const [modal, setModal] = useRecoilState(modalState);
+  const [, closeModall] = useModal();
+  const [modal] = useRecoilState(modalState);
 
-  const handleClose = () => {
-    setModal(null);
+  const handleBackground = (e: any) => {
+    if (e.target !== e.currentTarget) return;
+    closeModall();
   };
-
-  useEffect(() => {
-    window.addEventListener('popstate', handleClose);
-    return () => {
-      window.removeEventListener('popstate', handleClose);
-    };
-  });
 
   return (
     modal && (
-      <div className="fixed top-0 flex  h-full w-full max-w-[calc(100vh*0.6)] items-center justify-center bg-slate-400/50 ">
+      <div
+        className="fixed top-0 flex  h-full w-full max-w-[calc(100vh*0.6)] items-center justify-center bg-slate-400/50"
+        onClick={handleBackground}
+      >
         <div className="relative flex h-[50%] basis-4/5 flex-col items-center justify-center bg-red-300">
           {modal.modalComponent}
         </div>
