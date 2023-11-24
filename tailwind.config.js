ae3180c02129fe67071b101f8f5f4283cb64e403
arkingco <arkingco@gmail.com>
2023-11-25 02:53:39 +0900

    feat: strcat 테마별 색상 tailwind 적용#72

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,34 @@ module.exports = {
         'strcat-blue': '#557FE4',
         'strcat-yellow': '#FBFF36',
         'strcat-buttonColor': '#8F38FF',
+
+        'strcat-default-yellow': '#FBFF36',
+        'strcat-default-cyan': '#6CD8ED',
+        'strcat-default-green': '#7CD43',
+        'strcat-default-magenta': '#FF43A8',
+        'strcat-default-white': '#FFFFFF',
+        'strcat-default-black': '#212121',
+
+        'strcat-calm-orange': '#FFA857',
+        'strcat-calm-cyan': '#ABC4FF',
+        'strcat-calm-text-cyan': '567FE4',
+        'strcat-calm-green': '#BEE4A4',
+        'strcat-calm-text-green': '#61A136',
+        'strcat-calm-magenta': '#FF43A8',
+        'strcat-calm-white': '#F0EEEA',
+        'strcat-calm-black': '#463F3A',
+
+        'strcat-green-black': '#212121',
+        'strcat-green-cyan': '#8CD8ED',
+        'strcat-green-yellow': '#FBFF36',
+        'strcat-green-green': '#7CED43',
+
+        'strcat-cyan-white': '#FFFFFF',
+        'strcat-cyan-green': '#7CED43',
+        'strcat-cyan-yellow': '#FBFF36',
+        'strcat-cyan-magenta': '#FF43A8',
+        'strcat-cyan-cyan': '#6CD8ED',
+        'strcat-cyan-black': '#212121',
       },
       keyframes: {
         slide: {
