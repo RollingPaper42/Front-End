113d3c77d3298e595d4611b305d18d2f0ae60a92
lyssoi <114637463+lyssoi@users.noreply.github.com>
2023-11-24 13:59:19 +0900

    fix: writer태그 주석 해제 #62

--- src/component/ObserveContent.tsx
+++ src/component/ObserveContent.tsx
@@ -59,13 +59,13 @@ const ObserveContent = ({ content, boardId, isAdd }: props) => {
       >
         {content.text}
       </div>
-      {/* {!isAdd &&
+      {!isAdd &&
         observe.boardId === boardId &&
         observe.contentId === content.id && (
           <div
-            className={`bg-strcat-green animate-slide absolute right-[24px]  mt-[1px] px-1 text-white`}
+            className={`bg-strcat-green absolute right-[24px] mt-[1px]  animate-slide px-1 text-white`}
           >{`From: ${observe.writer}`}</div>
-        )} */}
+        )}
     </div>
   );
 };
