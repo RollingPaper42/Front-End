965d15ff32e67f8ccff464a6d1c38b9134f4862d
dokoh <dokoh@dokohui-MacBookAir.local>
2023-11-25 21:07:54 +0900

    feat/summary 페이지 css 및 스크롤 구현

--- src/app/summary/page.tsx
+++ src/app/summary/page.tsx
@@ -8,6 +8,7 @@ import ThemeChange from '@/component/ThemeChange';
 import { axiosInstance } from '@/utils/axios';
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import { title } from 'process';
 
 export default function Home() {
   const [Theme, setTheme] = useRecoilState(themeState);
@@ -35,42 +36,71 @@ export default function Home() {
   // }, []);
 
   return (
-    <div className={`${Theme.BgColor}`}>
-      <div className="fixed max-w-[calc(100vh*0.6)]">
-        <Link href="/board">
-          <Image
-            src="/backpage.png"
-            width={24}
-            height={24}
-            alt="backpagebutton"
-            className="left-[30px] top-[18px] "
-          />
-        </Link>
-        <div className={`top-[20px] mx-[25px]  ${Theme.DefaultFontColor} `}>
-          스트링캣 공유하기
+    <div className={`${Theme.background}`}>
+      <div className=" fixed flex h-full w-full max-w-[calc(100vh*0.6)]  flex-col">
+        <div className=" basis-3/12">
+          <div className="flex h-full w-full flex-col">
+            <div className=" basis-2/5">
+              <div className=" flex h-full w-full flex-row">
+                <div className=" basis-1/6 items-center justify-center">
+                  <Image
+                    src="/backpage.png"
+                    width={24}
+                    height={24}
+                    alt="backpagebutton"
+                    className="ml-[24px] mt-[16px]"
+                  />
+                </div>
+                <div className=" basis-4/6">
+                  <div
+                    className={`text-center text-[18px] ${Theme.defaultText} mt-[16px]`}
+                  >
+                    스트링캣 공유하기
+                  </div>
+                </div>
+                <div className=" basis-1/6"></div>
+              </div>
+            </div>
+            <div className="mx-[25px]  mt-[7px] basis-3/5">
+              <div className={`text-[22px] ${Theme.defaultText}`}>{Title}</div>
+            </div>
+          </div>
         </div>
-        <div
-          className={`top-[104px] mx-[25px]  text-xl ${Theme.DefaultFontColor}`}
-        >
-          {Title}
+        <div className=" basis-7/12 ">
+          <div className=" flex h-full w-full flex-col ">
+            <div className=" basis-1/4"></div>
+            <div className=" basis-1/4">
+              <div className="flex h-full w-full flex-row">
+                <div className="basis-1/2">
+                  <div
+                    className={`${Theme.defaultText} mx-[25px] text-[26px] `}
+                  >
+                    총 {ContentCount}번의 <br /> 마음으로 <br /> 내 스트링캣이{' '}
+                    <br /> 총 {ContentTextCount}자 <br /> 이어졌어요!
+                  </div>
+                </div>
+                <div className="basis-1/2"></div>
+              </div>
+            </div>
+            <div className=" basis-1/4 "></div>
+            <div className=" basis-1/4"></div>
+          </div>
         </div>
-        <div
-          className={`top-[300px] -z-0   text-2xl ${Theme.DefaultFontColor} `}
-        >
-          총 {ContentCount}번의 <br /> 마음으로 <br /> 내 스트링캣이 <br /> 총{' '}
-          {ContentTextCount}자 <br /> 이어졌어요!
+        <div className=" mx-[25px] basis-2/12">
+          <div className="flex flex-row items-center justify-center">
+            <button></button>
+          </div>
         </div>
       </div>
-
-      <div className="flex flex-row">
-        <div className="h-full w-full basis-2/4 bg-red-600"> text </div>
-        <div className="flex basis-2/4 content-start justify-start">
+      <div className="flex h-full w-full flex-row">
+        <div className="basis-1/2"></div>
+        <div className="basis-1/2">
           <Image
             src="/strcatImage.png"
             width={153}
             height={1040}
             alt="Image"
-            className=" relative  -z-0"
+            className="mr-[24px] mt-[186px]"
           />
         </div>
       </div>
