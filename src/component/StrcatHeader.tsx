1c2e29596e54c13f4519cfc3c77e7c0ad09353e3
lamPolar <lovast7@gmail.com>
2023-11-27 01:17:00 +0900

    style : header fixed 로 변경 #98

--- src/component/StrcatHeader.tsx
+++ src/component/StrcatHeader.tsx
@@ -17,50 +17,52 @@ export default function StrcatHeader() {
   }, [checkLogin]);
 
   return (
-    <div
-      className={`flex h-[56px] flex-row items-center justify-between ${theme.background} px-[24px]`}
-    >
-      <Link href="/">
-        <Image
-          src={`/Logo_${theme.name}.svg`}
-          width={89}
-          height={39}
-          alt="logo"
-          priority
-          loading="eager"
-        />
-      </Link>
-      <div className="basis-4/6"></div>
-      {isLogin ? (
-        <Image
-          src={`/ProfileImg_${theme.name}.svg`}
-          width={24}
-          height={24}
-          alt="profileImg"
-          loading="eager"
-          priority
-          onClick={() => setDrawer(true)}
-        />
-      ) : (
-        <Link href="/login">
-          <div className="relative h-[34px] w-[74px]">
-            <Image
-              src={`/LoginButton_${theme.name}.svg`}
-              width={74}
-              height={34}
-              alt="login"
-              loading="eager"
-              className="absolute inset-0"
-              priority
-            />
-            <span
-              className={`absolute inset-0 flex items-center justify-center ${theme.defaultText}`}
-            >
-              로그인
-            </span>
-          </div>
+    <div className="fixed top-0 z-10 w-full max-w-md">
+      <div
+        className={`flex h-[56px] flex-row items-center justify-between ${theme.background} px-[24px]`}
+      >
+        <Link href="/">
+          <Image
+            src={`/Logo_${theme.name}.svg`}
+            width={89}
+            height={39}
+            alt="logo"
+            priority
+            loading="eager"
+          />
         </Link>
-      )}
+        <div className="basis-4/6"></div>
+        {isLogin ? (
+          <Image
+            src={`/ProfileImg_${theme.name}.svg`}
+            width={24}
+            height={24}
+            alt="profileImg"
+            loading="eager"
+            priority
+            onClick={() => setDrawer(true)}
+          />
+        ) : (
+          <Link href="/login">
+            <div className="relative h-[34px] w-[74px]">
+              <Image
+                src={`/LoginButton_${theme.name}.svg`}
+                width={74}
+                height={34}
+                alt="login"
+                loading="eager"
+                className="absolute inset-0"
+                priority
+              />
+              <span
+                className={`absolute inset-0 flex items-center justify-center ${theme.defaultText}`}
+              >
+                로그인
+              </span>
+            </div>
+          </Link>
+        )}
+      </div>
     </div>
   );
 }
