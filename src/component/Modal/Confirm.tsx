5533aeea83c3f99a6e61a87bfb9cfd1759fc2ca2
lyssoi <114637463+lyssoi@users.noreply.github.com>
2023-11-27 21:11:00 +0900

    feat/confirm모달 디자인 적용 #74

--- src/component/Modal/Confirm.tsx
+++ src/component/Modal/Confirm.tsx
@@ -1,4 +1,5 @@
 import BottomButton from '@/component/BottomButton';
+import Image from 'next/image';
 
 interface Props {
   content: string;
@@ -8,35 +9,32 @@ interface Props {
 
 export default function Confirm({ content, yes, no }: Props) {
   return (
-    <div className="flex  h-[312px] w-[311px] flex-col bg-green-700">
-      <div
-        className={`'bg-[#CCCCCC]' } absolute inset-0 h-[308px] 
-        w-full`}
-      >
-        <div
-          className={`w-full'bg-[#CCCCCC]' } relative bottom-[4.5px] left-[2px]
-          h-[38px]`}
-          style={{ lineHeight: '3rem' }}
-        />
-      </div>
-      {content}
-      <div className="flex flex-row justify-around">
-        <BottomButton
-          height="h-[42px]"
-          color="bg-white"
-          name="예"
-          width="basis-2/5"
-          onClickHandler={yes}
-          disabled={false}
-        />
-        <BottomButton
-          height="h-[42px]"
-          color="bg-white"
-          name="아니요"
-          width="basis-2/5"
-          onClickHandler={no}
-          disabled={false}
-        />
+    <div className="flex  h-[312px] w-[311px] flex-col">
+      <Image src={'/Modal.png'} alt="modal" width={312} height={312} />
+      <div className="felx felx-col absolute h-[312px] w-[312px] items-center justify-center">
+        <div className=" flex h-[270px] w-full items-center justify-center">
+          <p>{content}</p>
+        </div>
+        <div className="absolute bottom-[5px] w-[312px]">
+          <div className="bottom-5 flex flex-row justify-around">
+            <BottomButton
+              height="h-[42px]"
+              color="bg-red-300"
+              name="예"
+              width="basis-2/5"
+              onClickHandler={yes}
+              disabled={false}
+            />
+            <BottomButton
+              height="h-[42px]"
+              color="bg-yellow-300"
+              name="아니요"
+              width="basis-2/5"
+              onClickHandler={no}
+              disabled={false}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
