4379770e3ea5f13029d8f16aedbd5163c3a224a3
arkingco <arkingco@gmail.com>
2023-11-27 16:49:13 +0900

    fix: confirm 모달 디자인 수정 #74

--- src/component/Modal/Confirm.tsx
+++ src/component/Modal/Confirm.tsx
@@ -1,3 +1,5 @@
+import BottomButton from '@/component/BottomButton';
+
 interface Props {
   content: string;
   yes: () => void;
@@ -6,23 +8,35 @@ interface Props {
 
 export default function Confirm({ content, yes, no }: Props) {
   return (
-    <div className="flex h-full w-full flex-col bg-green-700">
-      <div className="flex h-[70%] items-center justify-center p-10 text-center text-2xl">
-        {content}
+    <div className="flex  h-[312px] w-[311px] flex-col bg-green-700">
+      <div
+        className={`'bg-[#CCCCCC]' } absolute inset-0 h-[308px] 
+        w-full`}
+      >
+        <div
+          className={`w-full'bg-[#CCCCCC]' } relative bottom-[4.5px] left-[2px]
+          h-[38px]`}
+          style={{ lineHeight: '3rem' }}
+        />
       </div>
-      <div className="flex h-[30%] flex-row justify-around">
-        <button
-          className="h-[80%] basis-2/5 bg-amber-200 text-3xl"
-          onClick={yes}
-        >
-          예
-        </button>
-        <button
-          className="h-[80%] basis-2/5 bg-amber-200 text-3xl"
-          onClick={no}
-        >
-          아니요
-        </button>
+      {content}
+      <div className="flex flex-row justify-around">
+        <BottomButton
+          height="h-[42px]"
+          color="bg-white"
+          name="예"
+          width="basis-2/5"
+          onClickHandler={yes}
+          disabled={false}
+        />
+        <BottomButton
+          height="h-[42px]"
+          color="bg-white"
+          name="아니요"
+          width="basis-2/5"
+          onClickHandler={no}
+          disabled={false}
+        />
       </div>
     </div>
   );
