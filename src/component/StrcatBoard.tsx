388c389495dcb113026af61166510aa8de2ff135
lyssoi <114637463+lyssoi@users.noreply.github.com>
2023-11-27 13:40:36 +0900

    feat: font 제거

--- src/component/StrcatBoard.tsx
+++ src/component/StrcatBoard.tsx
@@ -33,10 +33,7 @@ const StrcatBoard = forwardRef<HTMLDivElement, Props>(function StrcatBoard(
     setContent(board.contents);
   }, [board]);
   return (
-    <div
-      ref={ref}
-      className={` font-FiraCode ${theme.background} px-[24px] duration-200`}
-    >
+    <div ref={ref} className={` ${theme.background} px-[24px] duration-200`}>
       <ObserveTitle isAdd={isAdd} board={board} />
       <div className={`z-0 inline`}>
         {content &&
