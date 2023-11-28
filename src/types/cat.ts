767ced12cd5113899f6ba14cb053c9101535634a
arkingco <arkingco@gmail.com>
2023-11-28 21:00:51 +0900

    feat: 각 애니메이션에 해당하는 gif 사이즈, 위치, src이름 타입 파일에 분리#18

--- src/types/cat.ts
+++ src/types/cat.ts
@@ -14,3 +14,20 @@ export interface cat {
   width: number;
   height: number;
 }
+
+export const catAnimationDetail = {
+  [catAction.exit]: {
+    src: '/cats/exit.gif',
+    width: 114,
+    height: 40,
+    top: 8,
+    left: 0,
+  },
+  [catAction.sit]: {
+    src: '/cats/sit.gif',
+    width: 40,
+    height: 40,
+    top: -40,
+    left: 200,
+  },
+};
