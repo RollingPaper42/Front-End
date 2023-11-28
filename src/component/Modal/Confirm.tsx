50c8063756b583e8353e9ec43e4dfca7a1cbea47
lyssoi <114637463+lyssoi@users.noreply.github.com>
2023-11-28 14:50:45 +0900

    feat: 모달 색변경 #74

--- src/component/Modal/Confirm.tsx
+++ src/component/Modal/Confirm.tsx
@@ -24,18 +24,18 @@ export default function Confirm({ content, yes, no }: Props) {
         <div className="flex flex-row justify-around">
           <BottomButton
             height="h-[42px]"
-            color="bg-red-300"
-            name="예"
+            color="bg-[#A6A6A6]"
+            name="아니요"
             width="basis-2/5"
-            onClickHandler={yes}
+            onClickHandler={no}
             disabled={false}
           />
           <BottomButton
             height="h-[42px]"
-            color="bg-yellow-300"
-            name="아니요"
+            color="bg-[#7CED43]"
+            name="예"
             width="basis-2/5"
-            onClickHandler={no}
+            onClickHandler={yes}
             disabled={false}
           />
         </div>
