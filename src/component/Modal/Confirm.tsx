9b379246268fcb283f166269b84ce6336f59e9fd
lyssoi <114637463+lyssoi@users.noreply.github.com>
2023-11-28 14:18:52 +0900

    feat/모달 컴포넌트 닫기버튼 추가#74

--- src/component/Modal/Confirm.tsx
+++ src/component/Modal/Confirm.tsx
@@ -1,5 +1,6 @@
 import BottomButton from '@/component/BottomButton';
 import Image from 'next/image';
+import Close from '../Icon/Close';
 
 interface Props {
   content: string;
@@ -12,28 +13,31 @@ export default function Confirm({ content, yes, no }: Props) {
     <div className="flex  h-[312px] w-[311px] flex-col">
       <Image src={'/Modal.png'} alt="modal" width={312} height={312} />
       <div className="felx felx-col absolute h-[312px] w-[312px] items-center justify-center">
-        <div className=" flex h-[270px] w-full items-center justify-center">
+        <div className="flex w-full justify-end p-[16px]">
+          <div onClick={no}>
+            <Close />
+          </div>
+        </div>
+        <div className=" flex h-[190px] items-center justify-center">
           <p>{content}</p>
         </div>
-        <div className="absolute bottom-[5px] w-[312px]">
-          <div className="bottom-5 flex flex-row justify-around">
-            <BottomButton
-              height="h-[42px]"
-              color="bg-red-300"
-              name="예"
-              width="basis-2/5"
-              onClickHandler={yes}
-              disabled={false}
-            />
-            <BottomButton
-              height="h-[42px]"
-              color="bg-yellow-300"
-              name="아니요"
-              width="basis-2/5"
-              onClickHandler={no}
-              disabled={false}
-            />
-          </div>
+        <div className="flex flex-row justify-around">
+          <BottomButton
+            height="h-[42px]"
+            color="bg-red-300"
+            name="예"
+            width="basis-2/5"
+            onClickHandler={yes}
+            disabled={false}
+          />
+          <BottomButton
+            height="h-[42px]"
+            color="bg-yellow-300"
+            name="아니요"
+            width="basis-2/5"
+            onClickHandler={no}
+            disabled={false}
+          />
         </div>
       </div>
     </div>
