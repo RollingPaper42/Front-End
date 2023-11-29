4ad00975354656b5254ec7e6273cfec41ec2fbd5
lamPolar <lovast7@gmail.com>
2023-11-29 14:53:30 +0900

    fix : personal 이전 파일로 롤백

--- src/app/personal/[id]/page.tsx
+++ src/app/personal/[id]/page.tsx
@@ -45,7 +45,7 @@ export default function Personal({ params }: { params: { id: string } }) {
       <Drawer />
       <StrcatHeader />
       <div
-        className={`relative h-full w-full py-[24px] text-justify ${theme.background} pb-[500px]`}
+        className={`relative w-full py-[24px] text-justify ${theme.background} pb-[500px]`}
       >
         {board.length && (
           <StrcatBoard
@@ -80,7 +80,7 @@ export default function Personal({ params }: { params: { id: string } }) {
                   height="h-[42px]"
                   width="basis-3/5"
                   onClickHandler={handleClick}
-                  disabled={false}
+                  disabled={!observe.boardId}
                   color={`bg-strcat-cyan`}
                 />
               </div>
