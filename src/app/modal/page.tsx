56d1165fabce18d08a3f7245d95ecb6272489bdc
lyssoi <114637463+lyssoi@users.noreply.github.com>
2023-11-30 15:16:01 +0900

    빌드에러해결

--- src/app/modal/page.tsx
+++ src/app/modal/page.tsx
@@ -29,52 +29,6 @@ export default function Modal() {
       />,
     );
   };
-
-  const handleCatTop = () => {
-    const div = document.getElementById('catdiv1');
-    if (div === null) return;
-    const divLeft = div.getBoundingClientRect().left;
-    const divTop = div.getBoundingClientRect().top;
-    setCat({
-      src: '/cats/in.gif',
-      catAction: catAction.out,
-      width: 50,
-      height: 50,
-      top: divTop,
-      left: divLeft,
-    });
-  };
-
-  const handleCatRight = () => {
-    const div = document.getElementById('catdiv2');
-    if (div === null) return;
-    const divBottom = div.getBoundingClientRect().bottom;
-    const divRight = div.getBoundingClientRect().right;
-    setCat({
-      src: '/cats/strcat/in.gif',
-      catAction: catAction.out,
-      width: 50,
-      height: 50,
-      top: divBottom,
-      left: divRight - 50,
-    });
-  };
-
-  const handleCatBottom = () => {
-    const div = document.getElementById('catdiv3');
-    if (div === null) return;
-    const divLeft = div.getBoundingClientRect().left;
-    const divBottom = div.getBoundingClientRect().bottom;
-    setCat({
-      src: '/cats/strcat/in.gif',
-      catAction: catAction.out,
-      width: 50,
-      height: 50,
-      top: divBottom + 50,
-      left: divLeft,
-    });
-  };
-
   const handleErrorRespons = () => {
     axiosInstance
       .get('/api/error')
@@ -90,15 +44,6 @@ export default function Modal() {
       </div>
       <div>
         <button onClick={handleConfirm}>Confirm Click</button>
-      </div>
-      <div id="catdiv1">
-        <button onClick={handleCatTop}>handleCatTop</button>
-      </div>
-      <div id="catdiv2">
-        <button onClick={handleCatRight}>handleCatRight</button>
-      </div>
-      <div id="catdiv3">
-        <button onClick={handleCatBottom}>handleCatBottom</button>
       </div>
       <div>
         <button onClick={handleErrorRespons}>500 Error api send</button>
