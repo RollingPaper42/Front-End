b9e21be043ae359c492c7946c7c7987f3d43d9e9
lyssoi <114637463+lyssoi@users.noreply.github.com>
2023-12-01 15:53:36 +0900

    feat: svg 카멜케이스로 변경 #144

--- src/component/Icon/ModalBackground.tsx
+++ src/component/Icon/ModalBackground.tsx
@@ -8,9 +8,9 @@ export default function ModalBackground() {
     >
       <path
         fill="#fff"
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M312 0H3v4H0v308h309v-4h3V0Z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </svg>
   );
