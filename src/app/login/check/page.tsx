95573942c5fcc15e5e2b9925d22ce8aeeef2fee8
arkingco <arkingco@gmail.com>
2023-12-11 04:09:38 +0900

    refactor: login check 페이지 npm run build 에러 수정#209

--- src/app/login/check/page.tsx
+++ src/app/login/check/page.tsx
@@ -8,15 +8,14 @@ export default function Check() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const token = searchParams.get('token');
-  if (typeof window === 'undefined') return;
-  localStorage.setItem('strcat_token', token ?? '');
-  const url = localStorage.getItem('strcat_login_success_url');
   useEffect(() => {
+    localStorage.setItem('strcat_token', token ?? '');
+    const url = localStorage.getItem('strcat_login_success_url');
     if (url) {
       router.push(url);
       // localStorage.removeItem('strcat_login_success_url');
     } else {
       router.push('/');
     }
-  }, [router, url]);
+  }, [router]);
 }
