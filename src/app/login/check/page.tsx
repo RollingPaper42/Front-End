3e1666b080a720b132b0eee61a26c06a1f8f50bd
arkingco <arkingco@gmail.com>
2023-12-11 04:03:58 +0900

    refactor: check 페이지 localstorage 에러 해결#209

--- src/app/login/check/page.tsx
+++ src/app/login/check/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 
-import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
+
+import { useRouter, useSearchParams } from 'next/navigation';
 
 export default function Check() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const token = searchParams.get('token');
+  if (typeof window === 'undefined') return;
   localStorage.setItem('strcat_token', token ?? '');
   const url = localStorage.getItem('strcat_login_success_url');
   useEffect(() => {
