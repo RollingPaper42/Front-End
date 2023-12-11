b9306f40f0668f9367185105027b8354b0b4df50
lamPolar <lovast7@gmail.com>
2023-12-11 17:27:55 +0900

    feat : hamburger 아이콘 생성 및 export 추가 #219

--- src/component/Icon/Header.tsx
+++ src/component/Icon/Header.tsx
@@ -1,6 +1,7 @@
+import HamburgerMenu from './header/HamburgerMenu';
 import HeaderProfileCat from './header/HeaderProfileCat';
 import LogoCat from './header/LogoCat';
 import LogoText from './header/LogoText';
 import Outline from './header/Outline';
 
-export { HeaderProfileCat, LogoCat, LogoText, Outline };
+export { HeaderProfileCat, LogoCat, LogoText, Outline, HamburgerMenu };
