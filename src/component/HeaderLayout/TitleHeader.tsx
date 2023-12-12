98206fb4ce92cd04a8676d64618669d6648386ed
lamPolar <lovast7@gmail.com>
2023-12-12 22:13:04 +0900

    feat : scroll down시 헤더, 바텀 버튼 비활성화 #219

--- src/component/HeaderLayout/TitleHeader.tsx
+++ src/component/HeaderLayout/TitleHeader.tsx
@@ -3,15 +3,21 @@
 import { useRecoilState } from 'recoil';
 
 import { HamburgerMenu } from '../Icon/Header';
+import { useScroll } from '@/hooks/useScroll';
 import { drawerState, themeState, titleState } from '@/recoil/state';
 
 export default function TitleHeader() {
   const [title] = useRecoilState(titleState);
   const [, setDrawer] = useRecoilState(drawerState);
   const [theme] = useRecoilState(themeState);
+  const isHidden = useScroll();
 
   return (
-    <div className="fixed top-0 z-10 w-full max-w-md">
+    <div
+      className={`fixed top-0 z-10 w-full max-w-md transition-transform duration-300 ${
+        isHidden ? '-translate-y-full' : 'translate-y-0'
+      }`}
+    >
       <div
         className={`flex min-h-[52px] py-[10px] flex-row items-stretch justify-between ${theme.bgTheme.background} px-[24px]`}
         id="titleHeader"
---
+++ src/hooks/useScroll.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+export const useScroll = () => {
+  const [isHidden, setIsHidden] = useState(false);
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollTop = window.scrollY;
+      if (currentScrollTop > lastScrollTop) {
+        setIsHidden(true);
+      } else {
+        setIsHidden(false);
+      }
+      setLastScrollTop(currentScrollTop);
+    };
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [lastScrollTop]);
+
+  return isHidden;
+};
