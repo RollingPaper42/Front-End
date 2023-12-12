fe0e35567ea314271e2b0186d524306dc6daac0d
arkingco <arkingco@gmail.com>
2023-12-13 02:30:07 +0900

    feat: 메인 페이지 리뉴얼#240

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@
 
 import { useRecoilState } from 'recoil';
 
-import LongCat from '@/component/Icon/LongCat';
+import MainManStrcat from '@/component/MainManStrcat';
 import { useLogin } from '@/hooks/useLogin';
-import { titleFont } from '@/recoil/font';
 import { themeState } from '@/recoil/theme/theme';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 export default function Home() {
@@ -13,7 +13,7 @@ export default function Home() {
   const [theme] = useRecoilState(themeState);
   const router = useRouter();
 
-  const handlePersonalStrcat = () => {
+  const handleClickPersonal = () => {
     if (isLogin) router.push('create');
     else {
       localStorage.setItem('strcat_login_success_url', '/create');
@@ -21,90 +21,49 @@ export default function Home() {
     }
   };
 
-  const handleGroupStrcat = () => {
-    if (isLogin) router.push('create/group');
-    else {
-      localStorage.setItem('strcat_login_success_url', '/create/group');
-      router.push('/login');
-    }
-  };
-
   return (
-    <div className={`${theme.bgTheme.background}`}>
-      <div className=" fixed flex h-full w-full max-w-md  flex-col">
-        <div className="basis-1/12"></div>
-        <div className="basis-6/12">
+    <div className={` bg-strcat-black h-auto min-h-full`}>
+      <div className="flex flex-col pt-[152px] items-center justify-center">
+        <Image
+          src="/StrcatIcon.svg"
+          width={42}
+          height={42}
+          alt="mainStrcatIcon"
+        />
+        <div className=" font-bold text-[28px] py-[11px] text-white">
+          스트링캣
+        </div>
+        <div className=" text-body-size2 text-white opacity-50">
+          함께 문장을 이어가는 롤링페이퍼
+        </div>
+        <div className="pt-[167px]  text-body-size2 text-white opacity-50 text-center">
+          내 롤링페이퍼에서 <br /> 친구들의 이야기를 듣고 싶다면
+        </div>
+        <div className="flex flex-row mt-[16px] w-[252px] h-[44px] bg-strcat-bright-yellow rounded-[5px] justify-center items-center">
           <div
-            className={`mx-[24px] mt-[25px] ${titleFont.category2} ${theme.textTheme.default}`}
+            className=" text-body-size2 font-bold"
+            onClick={handleClickPersonal}
           >
-            <p>
-              {`\/\/ 스트링캣은 문자열을 끝없이 늘려 스크롤을 만들 수 있는 신개념
-              롤링페이퍼 서비스 입니다.`}
-            </p>
-            <p>소중한 사람에게 스트링캣을 남겨보세요!</p>
+            스트링캣 시작하기
           </div>
-        </div>
-        <div className="basis-5/12">
-          <div className=" flex h-full w-full flex-col">
-            <div
-              className={`mx-[24px] mt-[100px] inline basis-1/2 ${titleFont.category1}`}
-            >
-              <div className="inline">
-                <button
-                  className={`relative h-[33px] w-[150px] items-center ${theme.bgTheme.leftCTA} ${titleFont.category1}`}
-                  onClick={handlePersonalStrcat}
-                >
-                  <div
-                    className={`relative bottom-[4.5px] left-[2px] h-[33px] w-[150px] ${titleFont.category1} ${theme.bgTheme.leftCTA}`}
-                  >
-                    <div className=" bottom-[-4.5px] left-[-2px]">
-                      <div className="relative top-1 select-none">
-                        스트링캣 만들기
-                      </div>
-                    </div>
-                  </div>
-                </button>
-              </div>
-              <div className={`inline ${theme.textTheme.explainLeftCTA}`}>
-                &nbsp;를 누르면 하나의 문자열을 할당받을 수 있어요. 링크를
-                공유해 문자열을 끝없이 이어보세요.
-              </div>
-            </div>
-            <div
-              className={`mx-[24px] inline basis-1/2 ${titleFont.category1}`}
-            >
-              <div className="inline">
-                <button
-                  className={`relative h-[33px] w-[200px] items-center ${theme.bgTheme.rightCTA} ${titleFont.category1}`}
-                  onClick={handleGroupStrcat}
-                >
-                  <div
-                    className={`relative bottom-[4.5px] left-[2px] h-[33px]  w-[200px] ${titleFont.category1} ${theme.bgTheme.rightCTA}`}
-                  >
-                    <div className="relative top-1 select-none">
-                      그룹 스트링캣 만들기
-                    </div>
-                    <div className=" bottom-[-4.5px] left-[-2px]"></div>
-                  </div>
-                </button>
-              </div>
-              <div
-                className={`inline ${titleFont.category1} ${theme.textTheme.explainRightCTA}`}
-              >
-                &nbsp;를 누르면 여러 문자열을 한 그룹으로 관리할 수 있어요.
-                주렁주렁~
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="flex h-full w-full flex-row">
-        <div className="basis-1/2"></div>
-        <div className="basis-1/2 pr-[24px] pt-[228px]">
-          <LongCat
-            bodyColor={theme.catTheme.mainCat}
-            eyeColor={theme.catTheme.mainCatEye}
+          <Image
+            src="/IconNext.svg"
+            width={24}
+            height={24}
+            alt="mainStrcatIcon"
           />
+        </div>
+        <div className="pt-[54px]">
+          <Image
+            className="animate-bounce"
+            src="/IconUnder.svg"
+            width={30}
+            height={21}
+            alt="IconUnder"
+          />
+        </div>
+        <div className="pt-[100px] pb-[800px]">
+          <MainManStrcat />
         </div>
       </div>
     </div>
---
+++ src/component/MainManStrcat.tsx
@@ -0,0 +1,65 @@
+import { board } from '@/types/boards';
+import StrcatBoard from './StrcatBoard';
+
+const board: board = {
+    id: 'string',
+    title: 'Sample Board',
+    theme: 'cyan',
+    contents: [
+      {
+        id: 1,
+        text: '스트링캣에 오신 것을 환영합니다!',
+        writer: 'kipark',
+        photoUrl: '',
+      },
+      {
+        id: 2,
+        text: '혹시 strcat을 들어보신 적 있나요? 만약 들어봤다면 당신은 혹시 개발자?',
+        writer: 'jayoon',
+        photoUrl: '',
+      },
+      {
+        id: 3,
+        text: '몰라도 괜찮아요! 처음 들어봤더라도 스트링캣을 사용할 때 아무런 문제가 되지 않습니다.',
+        writer: 'dokoh',
+        photoUrl: '',
+      },
+      {
+        id: 4,
+        text: 'strcat은 여러 문자열(string)을 이어붙이는 (concatenate) 기능을 가진 C언어의 함수랍니다!',
+        writer: 'heeskim',
+        photoUrl: '',
+      },
+      {
+        id: 5,
+        text: '여기서 착안한 스트링캣은 여러 사람들의 말 한마디 한마디를 줄줄이 이어붙여 주는 롤링페이퍼 서비스예요.',
+        writer: 'sol',
+        photoUrl: '',
+      },
+      {
+        id: 6,
+        text: '스트링캣이 여러분에게 새롭고 재밌는 경험을 드릴 수 있길 바라면서',
+        writer: 'sumilee',
+        photoUrl: '',
+      },
+	  {
+        id: 7,
+        text: '스트링캣팀 올림',
+        writer: '100Z',
+        photoUrl: '',
+      },
+	  {
+        id: 8,
+        text: '문의사항 및 신고: strcat@gmail.com',
+        writer: 'seongjki',
+        photoUrl: '',
+      },
+    ],
+};
+export default function MainManStrcat() {
+  return (
+    <div>
+      <StrcatBoard board={board}/>
+    </div>
+  );
+}
