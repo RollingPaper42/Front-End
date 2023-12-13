67f5f971518b1a2f7704a5967089c13f641980c5
arkingco <arkingco@gmail.com>
2023-12-13 15:51:28 +0900

    fix: main 페이지 설명글 theme 수정 #240

--- src/component/MainManStrcat.tsx
+++ src/component/MainManStrcat.tsx
@@ -4,7 +4,7 @@ import StrcatBoard from './StrcatBoard';
 const board: board = {
     id: 'string',
     title: 'Sample Board',
-    theme: 'cyan',
+    theme: 'strcat',
     contents: [
       {
         id: 1,
