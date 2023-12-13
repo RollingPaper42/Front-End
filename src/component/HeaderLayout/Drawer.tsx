96f46ef05354d6507e502298e48399c6b403b34c
lyssoi <114637463+lyssoi@users.noreply.github.com>
2023-12-13 12:22:15 +0900

    feat: 로그인 안했을때 drawer 추가 #223

--- src/component/HeaderLayout/Drawer.tsx
+++ src/component/HeaderLayout/Drawer.tsx
@@ -3,7 +3,7 @@ import { useCallback, useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 
 import BottomButton from '../BottomButton';
-import { DrawerProfileCat, Logout } from '../Icon/Drawer';
+import { Logout } from '../Icon/Drawer';
 import DrawerClose from '../Icon/drawer/DrawerClose';
 import Home from '../Icon/drawer/Home';
 import DrawerItem from './DrawerItem';
@@ -13,14 +13,15 @@ import { drawerState, themeState } from '@/recoil/state';
 import { drawerBoard } from '@/types/drawerBoard';
 import { axiosInstance } from '@/utils/axios';
 import { handleBackground } from '@/utils/handleBackground';
-import { useRouter } from 'next/navigation';
+import { useParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Drawer() {
-  const [isLogin, , setIsLogin] = useLogin();
+  const [isLogin, checkLogin, setIsLogin] = useLogin();
   const [drawer, setDrawer] = useRecoilState(drawerState);
   const [personalList, setPersonalList] = useState<drawerBoard[]>([]);
   const [theme] = useRecoilState(themeState);
   const router = useRouter();
+  const pathname = usePathname();
 
   const fetchData = useCallback(async () => {
     try {
@@ -49,6 +50,15 @@ export default function Drawer() {
     router.push('/');
   };
 
+  const handleLogin = () => {
+    setDrawer(false);
+    localStorage.setItem('strcat_login_success_url', pathname);
+    router.push('/login');
+  };
+  useEffect(() => {
+    checkLogin();
+  }, []);
+
   useEffect(() => {
     fetchData();
   }, [fetchData]);
@@ -68,7 +78,10 @@ export default function Drawer() {
         >
           <div className="flex h-[70px] w-full justify-start px-[24px] py-[22px]">
             <div className="absolute right-[24px]">
-              <div className="flex justify-center items-center w-[24px] h-[24px]">
+              <div
+                className="flex justify-center items-center w-[24px] h-[24px]"
+                onClick={() => setDrawer(false)}
+              >
                 <DrawerClose />
               </div>
             </div>
@@ -76,31 +89,60 @@ export default function Drawer() {
           <div
             className={`flex flex-col items-center ${theme.textTheme.default}`}
           >
-            <DropList
-              title="내 스트링캣"
-              list={personalList}
-              category="personal"
-            />
-            <div className="w-full px-[24px] mt-[12px]">
-              <BottomButton
-                name="새 스트링캣 만들기"
-                width="w-full"
-                onClickHandler={handleNewStrcat}
-                disabled={false}
-                color={`bg-strcat-bright-yellow`}
-                textColor="text-strcat-black"
-                height="h-[44px]"
-              />
-            </div>
+            {isLogin ? (
+              <>
+                <DropList
+                  title="내 스트링캣"
+                  list={personalList}
+                  category="personal"
+                />
+                <div className="w-full px-[24px] mt-[12px]">
+                  <BottomButton
+                    name="새 스트링캣 만들기"
+                    width="w-full"
+                    onClickHandler={handleNewStrcat}
+                    disabled={false}
+                    color={`bg-strcat-bright-yellow`}
+                    textColor="text-strcat-black"
+                    height="h-[44px]"
+                  />
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="flex justify-start w-full px-[24px] text-body-size1 font-semibold">
+                  <p>
+                    로그인을 하면
+                    <br />
+                    나만의 스트링캣을 만들고
+                    <br />
+                    관리할 수 있어요
+                  </p>
+                </div>
+                <div className="w-full px-[24px] mt-[12px]">
+                  <BottomButton
+                    name="로그인"
+                    width="w-full"
+                    onClickHandler={handleLogin}
+                    disabled={false}
+                    color={`bg-strcat-bright-yellow`}
+                    textColor="text-strcat-black"
+                    height="h-[44px]"
+                  />
+                </div>
+              </>
+            )}
             <div
               className={`absolute bottom-0 w-full ${theme.bgTheme.background} px-[24px]`}
             >
               <div className="h-[54px] w-full" onClick={handleHome}>
                 <DrawerItem title="홈으로" icon={<Home />} />
               </div>
-              <div className="h-[54px] w-full" onClick={handleLogout}>
-                <DrawerItem title="로그아웃" icon={<Logout />} />
-              </div>
+              {isLogin ? (
+                <div className="h-[54px] w-full" onClick={handleLogout}>
+                  <DrawerItem title="로그아웃" icon={<Logout />} />
+                </div>
+              ) : null}
             </div>
           </div>
         </div>
