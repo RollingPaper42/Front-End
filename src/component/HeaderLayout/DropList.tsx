5bcef7d88b7e870a0fcaffb6f0feea0d288f83c1
lamPolar <lovast7@gmail.com>
2023-12-15 17:56:49 +0900

    style : drawer list length 변경#281

--- src/component/HeaderLayout/DropList.tsx
+++ src/component/HeaderLayout/DropList.tsx
@@ -12,16 +12,16 @@ interface Props {
 export default function DropList({ list, category }: Props) {
   return (
     <>
-      <div className="flex py-[12px] w-full items-center justify-between px-[24px]">
-        <h1 className="text-body-size1 font-semibold select-none">
+      <div className="flex w-full items-center justify-between px-[24px] py-[12px]">
+        <h1 className="select-none text-body-size1 font-semibold">
           내 스트링캣
         </h1>
-        <div className=" text-gray-500 text-caption-size2 font-medium">
+        <div className=" cursor-default select-none text-caption-size2 font-medium text-gray-500">
           {list.length}
         </div>
       </div>
       {list.length ? (
-        <div className="flex max-h-[280px] h-full bg-[#1D1D1D] w-full flex-col overflow-scroll scrollbar-thumb-[#373737] scrollbar-thin scrollbar-thumb-rounded-[7px] ">
+        <div className="flex h-full max-h-[280px] w-full flex-col overflow-scroll bg-[#1D1D1D] scrollbar-thin scrollbar-thumb-[#373737] scrollbar-thumb-rounded-[7px] ">
           <DropListItem list={list} category={category} />
         </div>
       ) : null}
