75cbf4ea6e59dbd5993dcc833fd0ae31ee4c6a78
lamPolar <lovast7@gmail.com>
2023-12-15 15:59:40 +0900

    fix : confirm 모달 subcontent에러 수정

--- src/component/Modal/Confirm.tsx
+++ src/component/Modal/Confirm.tsx
@@ -1,7 +1,4 @@
-import ModalBackground from '../Icon/ModalBackground';
-import ModalClose from '../Icon/ModalClose';
 import BottomButton from '@/component/BottomButton';
-import { titleFont } from '@/recoil/font';
 
 interface Props {
   mainContent: string;
@@ -12,13 +9,13 @@ interface Props {
 
 export default function Confirm({ mainContent, subContent, yes, no }: Props) {
   return (
-    <div className="relative w-[280px] pt-[32px] pb-[16px] px-[16px] bg-strcat-textarea-bg rounded-[16px] shadow-[0px_4px_20px_0px_rgba(0,0,0,0.50)] font-medium">
+    <div className="relative w-[280px] rounded-[16px] bg-strcat-textarea-bg px-[16px] pb-[16px] pt-[32px] font-medium shadow-[0px_4px_20px_0px_rgba(0,0,0,0.50)]">
       <div className=" items-center justify-center">
         <p className={`text-center text-body-size1 text-strcat-white`}>
           {mainContent}
         </p>
-        {subContent ?? (
-          <p className="text-center text-body-size2 text-strcat-white/50 mt-[12px]">
+        {subContent && (
+          <p className="text-strcat-gray-500 mt-[12px] text-center text-body-size2">
             {subContent}
           </p>
         )}
