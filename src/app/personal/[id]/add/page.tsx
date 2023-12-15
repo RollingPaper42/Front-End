15e03f99e071770dc44b69669a672847479ee810
lamPolar <lovast7@gmail.com>
2023-12-15 11:25:36 +0900

    refactor : writer component 분리 #273

--- src/app/personal/[id]/add/page.tsx
+++ src/app/personal/[id]/add/page.tsx
@@ -103,24 +103,11 @@ export default function Add({ params }: { params: { id: string } }) {
           <div className="mb-[12px] mt-[20px] text-body-size2 font-semibold tracking-[-0.32px] text-[#BCBCBC] ">
             From
           </div>
-          <div className="h-[51px] pl-[16px] w-full flex items-center justify-center rounded-lg bg-white/10">
-            <input
-              className="text-body-size2 w-full h-full outline-none bg-transparent placeholder:text-white/50 text-white"
-              maxLength={11}
-              value={writer}
-              onChange={handleWriter}
-              placeholder="익명의 스트링캣"
-              onFocus={() => setIsHidden(true)}
-              onBlur={() => setIsHidden(false)}
-            />
-            <div
-              className={`pr-[16px] text-caption-size2 ${
-                writer.length > 10 ? 'text-[#DE6565]' : 'text-white/50'
-              }`}
-            >
-              {writer.length}/10
-            </div>
-          </div>
+          <Writer
+            writer={writer}
+            handleWriter={handleWriter}
+            setIsHidden={setIsHidden}
+          />
           <div className="pb-[154px]" />
         </div>
       </div>
