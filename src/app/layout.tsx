b7a1b7f71a05e73c9ec8f32b0ed680e73688c09b
lamPolar <lovast7@gmail.com>
2023-12-17 19:50:29 +0900

    feat : clarity script 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,17 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const clairtyCode = `(function (c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "k73o8q506m");`;
+
   return (
     <html className="h-full">
       <head>
+        <Script id="ms-clarity" strategy="afterInteractive">
+          {clairtyCode}
+        </Script>
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-NT9VXJJDMB" />
         <Script id="google-analytics">
           {`
