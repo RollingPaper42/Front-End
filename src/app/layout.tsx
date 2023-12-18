f54d258b606789fa18ac501dad773aa76fd3efe8
kipark <Arkingco@gmail.com>
2023-12-18 16:14:03 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,28 +15,23 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const clairtyCode = `(function (c,l,a,r,i,t,y){
-    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", ${process.env.NEXT_PUBLIC_CLARITY_ID});`;
-
-  const googleAnalyticsCode = `
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ID});
-  `;
-
   return (
     <html className="h-full">
       <head>
         <Script id="ms-clarity" strategy="afterInteractive">
-          {clairtyCode}
+          {`(function (c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", ${process.env.NEXT_PUBLIC_CLARITY_ID});`}
         </Script>
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ID}`}
         />
-        <Script id="google-analytics">{googleAnalyticsCode}</Script>
+        <Script id="google-analytics">{`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ID});`}</Script>
         <title>STRCAT : 글을 이어 만드는 롤링페이퍼</title>
         <OpenGraph />
         <Description />
