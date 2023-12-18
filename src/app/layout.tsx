d64fa0663fed546da16657a776a0eaa67522a211
lamPolar <lovast7@gmail.com>
2023-12-18 13:38:00 +0900

    refactor : ga, naverseo, clarity id env로 이동

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   const clairtyCode = `(function (c,l,a,r,i,t,y){
     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "k73o8q506m");`;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", ${process.env.NEXT_PUBLIC_CLARITY_ID});`;
 
   return (
     <html className="h-full">
@@ -26,14 +26,15 @@ export default function RootLayout({
         <Script id="ms-clarity" strategy="afterInteractive">
           {clairtyCode}
         </Script>
-        <Script src="https://www.googletagmanager.com/gtag/js?id=G-NT9VXJJDMB" />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ID}`}
+        />
         <Script id="google-analytics">
           {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
- 
-          gtag('config', 'G-NT9VXJJDMB');
+          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ID});
         `}
         </Script>
         <title>strcat : 글을 이어 만드는 롤링페이퍼</title>
@@ -43,7 +44,7 @@ export default function RootLayout({
         <meta
           key="naver"
           name="naver-site-verification"
-          content="c075ea66f42b086697fe2754f53f08b0ee624bc2"
+          content={process.env.NEXT_PUBLIC_NAVER_ID}
         />
       </head>
       <body className="h-full overscroll-none  bg-black/80">
