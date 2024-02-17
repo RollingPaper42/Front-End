85d25ba2b82864e4a4b762dcd4e4e458905ee3c1
lamPolar <lovast7@gmail.com>
2024-02-17 15:30:35 +0900

    feat : history 전송 기능 구현 #390

--- src/app/login/check/page.tsx
+++ src/app/login/check/page.tsx
@@ -4,10 +4,22 @@ import { useEffect } from 'react';
 
 import { useRouter, useSearchParams } from 'next/navigation';
 
+import { History } from '@/types/history';
+import { axiosPostUserHistory } from '@/utils/apiInterface';
+
 export default function Check() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const token = searchParams.get('token');
+
+  useEffect(() => {
+    const history = localStorage.getItem('history');
+    if (history) {
+      const historyArray: History[] = JSON.parse(history);
+      axiosPostUserHistory({ history: historyArray });
+      localStorage.removeItem('history');
+    }
+  }, []);
 
   useEffect(() => {
     localStorage.setItem('strcat_token', token ?? '');
