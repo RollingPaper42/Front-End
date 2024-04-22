481e7f4a071f680d8cec565ade128e86a06bb1d1
arkingco <akringco@gmail.com>
2024-04-22 13:17:36 +0900

    feat: 메타테그 추가 #395

--- src/component/Common/Description.tsx
+++ src/component/Common/Description.tsx
@@ -2,6 +2,8 @@ export default function Description() {
   return (
     <>
       <link rel="canonical" href="https://strcat.me" />
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta
         name="description"
         content="글을 이어 만드는 롤링페이퍼, strcat. 친구들과 즐거운 경험을 쌓아보세요!"
@@ -10,6 +12,7 @@ export default function Description() {
         name="keywords"
         content="롤링페이퍼,글,친구,연인,자랑,mbti,익명,strcat,개발자,공유,추억,직장,학교,생일,선물,rolling,paper,롤링,페이퍼"
       />
+      <meta name="author" content="STRCAT 팀" />
       <meta name="robots" content="index,follow" />
       <meta name="googlebot" content="index,follow" />
     </>
