d49ccf0c78a9aad2b9f32835e2c9ea0e2b23e55e
arkingco <arkingco@gmail.com>
2024-05-27 14:01:11 +0900

    feat: 마우스 가로 스크롤이벤트를 위한 swiper js 적용#405

--- src/component/OpenStrcatBoard.tsx
+++ src/component/OpenStrcatBoard.tsx
@@ -1,37 +1,42 @@
+import 'react';
+
 import Link from 'next/link';
 
 import OpenStrcat from './OpenStrcat';
 import { openBoard } from '@/types/openBoard';
 import { OpenBoardTextColor, OpenBoardThemeColor } from '@/types/theme';
+import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 interface Props {
   openBoard: openBoard[];
 }
 
 export default function OpenStrcatBoard({ openBoard }: Props) {
-  console.log();
   return (
-    <div className="w-full flex flex-row overflow-x-scroll pt-[14px] gap-[12px] scrollbar-thin scrollbar-track-strcat-textarea-bg scrollbar-thumb-strcat-bright-yellow">
-      {openBoard
-        .sort(() => 0.5 - Math.random())
-        .map((item, i) => {
+    <div className="w-full flex flex-row overflow-x-scroll pt-[14px] no-padding">
+      <Swiper width={162}>
+        {openBoard.map((item, i) => {
           return (
             <div key={'openBoard' + item.title + item.id}>
-              <Link href={`/personal/${item.id}`}>
-                <OpenStrcat
-                  id={item.id}
-                  OpenBoardThemeColor={`${OpenBoardThemeColor[item.theme]}`}
-                  OpenBoardTextColor={`${OpenBoardTextColor[item.theme]}`}
-                  title={item.title}
-                  contentCount={item.contentCount}
-                  contentTextCount={item.contentTextCount}
-                  lastContentCreatedAt={item.lastContentCreatedAt}
-                  theme={item.theme}
-                />
-              </Link>
+              <SwiperSlide>
+                <Link href={`/personal/${item.id}`}>
+                  <OpenStrcat
+                    id={item.id}
+                    OpenBoardThemeColor={`${OpenBoardThemeColor[item.theme]}`}
+                    OpenBoardTextColor={`${OpenBoardTextColor[item.theme]}`}
+                    title={item.title}
+                    contentCount={item.contentCount}
+                    contentTextCount={item.contentTextCount}
+                    lastContentCreatedAt={item.lastContentCreatedAt}
+                    theme={item.theme}
+                  />
+                </Link>
+              </SwiperSlide>
             </div>
           );
         })}
+      </Swiper>
     </div>
   );
 }
